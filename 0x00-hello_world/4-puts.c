80ebe98f2c2c00768709fdc3d09bcbc1e793be3d
NitionVR <kmaganyane83@gmail.com>
2023-02-16 19:38:59 -0800

    program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program prints a given line using the puts function
+ * Return: 0
+ */
+int main(void)
+{
+ puts("\"programming is like building a multilingual puzzle");
+ return (0);
+}
+
